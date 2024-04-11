f8006d5b3fd8a08e7f53de813773460463cb5f90
Antonio <antonioavila433@gmail.com>
2024-04-11 01:59:48 -0400

    Add visualization

--- spot_jumping.py
+++ spot_jumping.py
@@ -13,31 +13,42 @@ from pydrake.all import (
     JacobianWrtVariable,
     AddUnitQuaternionConstraintOnPlant,
     eq,
+    AddDefaultVisualization,
+    PiecewisePolynomial,
+    MeshcatVisualizer,
 )
 from underactuated.underactuated import ConfigureParser
 
 # Stand
-# meshcat = StartMeshcat()
+meshcat = StartMeshcat()
 # run_pid_control(meshcat)
 
 ###################################################################################################
 
-builder = RobotDiagramBuilder(time_step=1e-4)
-
-# parse Spot urdf and default position
-parser = builder.parser()
+robot_builder = RobotDiagramBuilder(time_step=1e-4)
+parser = robot_builder.parser()
+scene_graph = robot_builder.scene_graph()
 ConfigureParser(parser)
 (spot,) = parser.AddModelsFromUrl("package://underactuated/models/spot/spot.dmd.yaml")
 parser.AddModelsFromUrl("package://underactuated/models/littledog/ground.urdf")
-plant = builder.plant()
+plant = robot_builder.plant()
 plant.set_discrete_contact_approximation(DiscreteContactApproximation.kLagged)
 plant.Finalize()
 plant_ad = plant.ToAutoDiffXd()
-builder = builder.builder()
+builder = robot_builder.builder()
+visualizer = MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat=meshcat)
 
-# create contexts to work with
+diagram = robot_builder.Build()
+
 plant_context = plant.CreateDefaultContext()
 plant_context_ad = plant_ad.CreateDefaultContext()
+
+
+diagram_context = diagram.CreateDefaultContext()
+ctx = plant.GetMyContextFromRoot(diagram_context)
+plant.SetPositions(plant_context, plant.GetDefaultPositions())
+diagram.ForcedPublish(diagram_context)
+
 
 
 nq = plant.num_positions()
@@ -47,9 +58,16 @@ nf = 3 # 3d friction
 q0 = plant.GetDefaultPositions()
 effort_ub = plant.GetEffortUpperLimits()
 effort_lb = plant.GetEffortLowerLimits()
+position_ub = plant.GetPositionUpperLimits()
+position_lb = plant.GetPositionLowerLimits()
+velocity_ub = plant.GetVelocityUpperLimits()
+velocity_lb = plant.GetVelocityLowerLimits()
+accel_ub = plant.GetAccelerationUpperLimits()
+accel_lb = plant.GetAccelerationLowerLimits()
 h_min = 0.01
 h_max = 0.1
 h = 0.01
+mu = 1.0
 
 ##### Jump Optimization
 prog = MathematicalProgram()
@@ -58,9 +76,9 @@ N_launch = 21
 N_flight = 50
 N = N_launch + N_flight
 
-q = prog.NewContinuousVariables(rows=N+1, cols=nq, name="q")
-v = prog.NewContinuousVariables(rows=N+1, cols=nv, name="v")
-q_ddot = prog.NewContinuousVariables(rows=N, cols=nv, name="q_ddot")
+q = prog.NewContinuousVariables(rows=N, cols=nq, name="q")
+v = prog.NewContinuousVariables(rows=N, cols=nv, name="v")
+vd = prog.NewContinuousVariables(rows=N, cols=nv, name="vd")
 u = prog.NewContinuousVariables(rows=N, cols=nu, name="u")
 fl_friction = prog.NewContinuousVariables(rows=N, cols=nf, name="fl_friction")
 fr_friction = prog.NewContinuousVariables(rows=N, cols=nf, name="fr_friction")
@@ -71,11 +89,10 @@ rr_friction = prog.NewContinuousVariables(rows=N, cols=nf, name="rr_friction")
 h = prog.NewContinuousVariables(N, name="h")
 prog.AddBoundingBoxConstraint([h_min] * N, [h_max] * N, h)
 
-
 # Manipulator Equations
 def manipulator_equations(vars):
     assert vars.size == nq +  2*nv + 4*nf
-    q, v, qdd, f_fl, f_fr, f_rl, f_rr = np.split(vars, [
+    m_q, m_v, m_vd, f_fl, f_fr, f_rl, f_rr = np.split(vars, [
         nq, 
         nq + nv, 
         nq + 2*nv, 
@@ -88,8 +105,8 @@ def manipulator_equations(vars):
     m_plant, m_context = (plant_ad, plant_context_ad) if isinstance(vars[0], AutoDiffXd) else (plant, plant_context)
 
     # set state
-    m_plant.SetPositions(m_context, q)
-    m_plant.SetVelocities(m_context, v)
+    m_plant.SetPositions(m_context, m_q)
+    m_plant.SetVelocities(m_context, m_v)
 
     # matrices for manipulator equations
     M = m_plant.CalcMassMatrixViaInverseDynamics(m_context)
@@ -103,7 +120,7 @@ def manipulator_equations(vars):
     J_rl = calc_foot_jacobian(m_plant, m_context, m_plant.GetFrameByName("rear_left_lower_leg"), world_frame, [0,0,0])
     J_rr = calc_foot_jacobian(m_plant, m_context, m_plant.GetFrameByName("rear_right_lower_leg"), world_frame, [0,0,0])
 
-    return M@qdd + Cv - tauG - J_fl.T@f_fl - J_fr.T@f_fr - J_rl.T@f_rl - J_rr.T@f_rr # should be equal to 0
+    return M@m_vd + Cv - tauG - J_fl.T@f_fl - J_fr.T@f_fr - J_rl.T@f_rl - J_rr.T@f_rr # should be equal to 0
 
 def calc_foot_jacobian(plt, ctxt, foot_frame, world_frame, position_in_frame):
     J = plt.CalcJacobianTranslationalVelocity(
@@ -122,29 +139,81 @@ prog.AddLinearEqualityConstraint(v[0], np.zeros_like(v[0]))
 
 # final position
 prog.AddLinearEqualityConstraint(q[-1], q0)
-prog.AddLinearEqualityConstraint(v[-1], np.zeros_like(v[0]))
 
-# Final quaterinons
-AddUnitQuaternionConstraintOnPlant(plant, q[N, :], prog)
-AddUnitQuaternionConstraintOnPlant(plant_ad, q[N, :], prog)
+# feet forces
+# for n in range(N_launch):
+#     prog.AddLinearConstraint(fl_friction[n, 2] >= 0)
+#     prog.AddLinearConstraint(fr_friction[n, 2] >= 0)
+#     prog.AddLinearConstraint(rl_friction[n, 2] >= 0)
+#     prog.AddLinearConstraint(rr_friction[n, 2] >= 0)
+
+#     prog.AddLinearConstraint(fl_friction[n, 0] <= mu*fl_friction[n, 2])
+#     prog.AddLinearConstraint(fl_friction[n, 0] >= -mu*fl_friction[n, 2])
+#     prog.AddLinearConstraint(fl_friction[n, 1] <= mu*fl_friction[n, 2])
+#     prog.AddLinearConstraint(fl_friction[n, 1] >= -mu*fl_friction[n, 2])
+
+#     prog.AddLinearConstraint(fr_friction[n, 0] <= mu*fr_friction[n, 2])
+#     prog.AddLinearConstraint(fr_friction[n, 0] >= -mu*fr_friction[n, 2])
+#     prog.AddLinearConstraint(fr_friction[n, 1] <= mu*fr_friction[n, 2])
+#     prog.AddLinearConstraint(fr_friction[n, 1] >= -mu*fr_friction[n, 2])
+
+#     prog.AddLinearConstraint(rl_friction[n, 0] <= mu*rl_friction[n, 2])
+#     prog.AddLinearConstraint(rl_friction[n, 0] >= -mu*rl_friction[n, 2])
+#     prog.AddLinearConstraint(rl_friction[n, 1] <= mu*rl_friction[n, 2])
+#     prog.AddLinearConstraint(rl_friction[n, 1] >= -mu*rl_friction[n, 2])
+
+#     prog.AddLinearConstraint(rr_friction[n, 0] <= mu*rr_friction[n, 2])
+#     prog.AddLinearConstraint(rr_friction[n, 0] >= -mu*rr_friction[n, 2])
+#     prog.AddLinearConstraint(rr_friction[n, 1] <= mu*rr_friction[n, 2])
+#     prog.AddLinearConstraint(rr_friction[n, 1] >= -mu*rr_friction[n, 2])
 
 for n in range(N):
-    # manipulator eqations
-    vars = np.concatenate((q[n+1], v[n+1], q_ddot[n], fl_friction[n], fr_friction[n], rl_friction[n], rr_friction[n]))
-    prog.AddConstraint(manipulator_equations, lb=[0]*nv, ub=[0]*nv, vars=vars)
-
     # unit quaternions
-    AddUnitQuaternionConstraintOnPlant(plant, q[n, :], prog)
-    AddUnitQuaternionConstraintOnPlant(plant_ad, q[n, :], prog)
+    AddUnitQuaternionConstraintOnPlant(plant, q[n], prog)
+    AddUnitQuaternionConstraintOnPlant(plant_ad, q[n], prog)
+
+    # joint limits
+    for j in range(nq):
+        prog.AddLinearConstraint(q[n, j] >= position_lb[j])
+        prog.AddLinearConstraint(q[n, j] <= position_ub[j])
 
     # Actuator limits
     for i in range(nu):
         prog.AddLinearConstraint(u[n, i] >= effort_lb[i])
         prog.AddLinearConstraint(u[n, i] <= effort_ub[i])
 
+    # velocity, acceleration limits
+    for i in range(nv):
+        prog.AddLinearConstraint(v[n, i] >= velocity_lb[i])
+        prog.AddLinearConstraint(v[n, i] <= velocity_ub[i])
+
+        prog.AddLinearConstraint(vd[n, i] >= accel_lb[i])
+        prog.AddLinearConstraint(vd[n, i] <= accel_ub[i])
+
+for n in range(N-1):
     # velocity/accel constraints
     prog.AddConstraint(eq(q[n + 1][1:], q[n][1:] + h[n] * v[n + 1]))
-    prog.AddConstraint(eq(v[n + 1], v[n] + h[n] * q_ddot[n]))
+    prog.AddConstraint(eq(v[n + 1], v[n] + h[n] * vd[n]))
+
+    # manipulator eqations
+    vars = np.concatenate((q[n+1], v[n+1], vd[n], fl_friction[n], fr_friction[n], rl_friction[n], rr_friction[n]))
+    prog.AddConstraint(manipulator_equations, lb=[0]*nv, ub=[0]*nv, vars=vars)
+
+
+def feet_height(q):
+    m_plant, m_context = (plant_ad, plant_context_ad) if isinstance(q[0], AutoDiffXd) else (plant, plant_context)
+    ground_frame = m_plant.GetBodyByName("ground").body_frame()
+    m_plant.SetPositions(m_context, q)
+
+    fl = m_plant.CalcPointsPositions(m_context, m_plant.GetBodyByName("front_left_lower_leg").body_frame(), [0,0,0], ground_frame)
+    fr = m_plant.CalcPointsPositions(m_context, m_plant.GetBodyByName("front_right_lower_leg").body_frame(), [0,0,0], ground_frame)
+    rl = m_plant.CalcPointsPositions(m_context, m_plant.GetBodyByName("rear_left_lower_leg").body_frame(), [0,0,0], ground_frame)
+    rr = m_plant.CalcPointsPositions(m_context, m_plant.GetBodyByName("rear_right_lower_leg").body_frame(), [0,0,0], ground_frame)
+
+    return np.array([fl[-1], fr[-1], rl[-1], rr[-1]])
+
+for n in range(N_launch):
+    prog.AddConstraint(feet_height, lb=[0]*4, ub=[np.inf]*4, vars=q[n])
 
 
 solver = SnoptSolver()
@@ -152,8 +221,21 @@ solver = SnoptSolver()
 print("Solving")
 start = time.time()
 result = solver.Solve(prog)
-print(time.time() - start)
-
-
 print(result.is_success())
-print(result.GetSolution(q[-1]))
+print("Time to solve:", time.time() - start)
+
+# visualize
+print("visualizing")
+context = diagram.CreateDefaultContext()
+plant_context = plant.GetMyContextFromRoot(context)
+t_sol = np.cumsum(result.GetSolution(h))
+q_sol = PiecewisePolynomial.FirstOrderHold(t_sol, result.GetSolution(q).T)
+visualizer.StartRecording()
+t0 = t_sol[0]
+tf = t_sol[-1]
+for t in t_sol:
+    context.SetTime(t)
+    plant.SetPositions(plant_context, q_sol.value(t))
+    diagram.ForcedPublish(context)
+visualizer.StopRecording()
+visualizer.PublishRecording()
